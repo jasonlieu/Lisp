24b653299e2114ee4afcba4d29222ef4dae069bf
jasonlieu <jasonlieuthefirst@gmail.com>
2018-09-25 20:21:37 -0700

    add knight movement

--- Giraffe/Giraffe.lisp
+++ Giraffe/Giraffe.lisp
@@ -10,8 +10,18 @@
 ;   4   G   2
 ;10     3       7
 ;   9       8
+;To edit check order: cut and paste to desired IF statement
+;              (AND (<= 0 (- y 1))                                      
+;                   (NOT (MEMBER (LIST x (- y 1)) path :test #'equal)))
+;                   (Giraffe x (- y 1) (APPEND (LIST (LIST x y)) path))
 (DEFUN Giraffe(x y path) 
-    (IF (NOT (EQUAL 49 (LIST-LENGTH path)))
+;    (IF (NOT  (AND (EQUAL 49 (LIST-LENGTH path))        ;check moves if path >49   
+;              (OR  (AND (= x 0) (= y 1))                   ;
+;                   (AND (= x 1) (= y 0))
+;                   (AND (= x 1) (= y 2))
+;                   (AND (= x 2) (= y 2))
+;             )))                          ;x y cannot legally move to 0,0  (moves 2,3,7,8 from 0,0)
+    (IF (NOT (EQUAL 49 (LIST-LENGTH path)))                             
         (PROGN (IF (AND (<= 0 (- y 1))                                      ;check 1
                 (NOT (MEMBER (LIST x (- y 1)) path :test #'equal)))         ;check if not already in path
             (Giraffe x (- y 1) (APPEND (LIST (LIST x y)) path))             ;next move
@@ -24,18 +34,40 @@
             (IF (AND (<= 0 (- x 1))                                         ;check 4
                     (NOT (MEMBER (LIST (- x 1) y) path :test #'equal)))
                 (Giraffe (- x 1) y (APPEND (LIST (LIST x y)) path))
-            )))))
+            (IF (AND (>= 6 (+ x 1))                                         
+                    (<= 0 (- y 2))
+                    (NOT (MEMBER (LIST (+ x 1) (- y 2)) path :test #'equal)))   ;check 5 
+                (Giraffe (+ x 1) (- y 2) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (>= 6 (+ x 2))                                         
+                    (<= 0 (- y 1))
+                    (NOT (MEMBER (LIST (+ x 2) (- y 1)) path :test #'equal)))   ;check 6 
+                (Giraffe (+ x 2) (- y 1) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (>= 6 (+ x 2))                                         
+                    (>= 6 (+ y 1))
+                    (NOT (MEMBER (LIST (+ x 2) (+ y 1)) path :test #'equal)))   ;check 7 
+                (Giraffe (+ x 2) (+ y 1) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (>= 6 (+ x 1))                                         
+                    (>= 6 (+ y 2))
+                    (NOT (MEMBER (LIST (+ x 1) (+ y 2)) path :test #'equal)))   ;check 8 
+                (Giraffe (+ x 1) (+ y 2) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (<= 0 (- x 1))                                         
+                    (>= 6 (+ y 2))
+                    (NOT (MEMBER (LIST (- x 1) (+ y 2)) path :test #'equal)))   ;check 9 
+                (Giraffe (- x 1) (+ y 2) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (<= 0 (- x 2))                                         
+                    (>= 6 (+ y 1))
+                    (NOT (MEMBER (LIST (- x 2) (+ y 1)) path :test #'equal)))   ;check 10 
+                (Giraffe (- x 2) (+ y 1) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (<= 0 (- x 2))                                         
+                    (<= 0 (- y 1))
+                    (NOT (MEMBER (LIST (- x 2) (- y 1)) path :test #'equal)))   ;check 11 
+                (Giraffe (- x 2) (- y 1) (APPEND (LIST (LIST x y)) path))
+            (IF (AND (<= 0 (- x 1))                                         
+                    (<= 0 (- y 2))
+                    (NOT (MEMBER (LIST (- x 1) (- y 2)) path :test #'equal)))   ;check 12 
+                (Giraffe (- x 1) (- y 2) (APPEND (LIST (LIST x y)) path))
+            )))))))))))))    ;bunch of nested IF's
             (PRINT (LIST "path:" path)))
 )
-;    (IF (AND (<= 0 (- x 1))          ;check 1
-;             (<= 0 (- y 2))
-;             (NOT (MEMBER (LIST x y) (LIST path) :test #'equal)))
-;        (Giraffe (- x 1) (- y 2) (APPEND (LIST (LIST x y)) path))
-;        (PRINT "nah on 1")
-;    )
-;    (IF (AND (>= 6 (+ x 1))          ;check 2
-;             (<= 0 (- y 2)))
-;        (PRINT "move 2")
-;        (PRINT "nah on 2")
-;    )
+
 (Giraffe 0 0 (LIST ""))      ;function call. Start at (0,0) with empty path
